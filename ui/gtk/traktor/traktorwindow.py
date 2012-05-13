669733955515dc8fae6c4ccd9ae989eccfd04b55
illuque <iago.lluque@gmail.com>
2012-05-13 23:20:12 +0200

    added combo and entry

--- ui/gtk/traktor/traktorwindow.py
+++ ui/gtk/traktor/traktorwindow.py
@@ -1,4 +1,5 @@
 from gi.repository import Gtk
+from . import constants
 
 MENU_UI = '''
 <ui>
@@ -23,6 +24,14 @@ class TraktorWindow(Gtk.Window):
         self.ui_manager = self._setup_ui_manager()
         menu_bar = self.ui_manager.get_widget('/MenuBar')
         box.pack_start(menu_bar, False, False, 0)
+
+        search_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        box.pack_start(search_box, False, False, 0)
+
+        self.program_combo = self._get_combo()
+        search_box.pack_start(self.program_combo, False, False, 0)
+        program_entry = self._get_entry()
+        search_box.pack_start(program_entry, False, False, 0)
 
         self.connect('delete-event', self._quit)
 
@@ -53,3 +62,30 @@ class TraktorWindow(Gtk.Window):
     def run(self):
         self.show_all()
         Gtk.main()
+
+    def _get_combo(self):
+        tv_programs = [(`constants.SEARCH_ALL_TYPE`, 'All'), (`constants.SEARCH_EPISODES_TYPE`, 'Episodes'), (`constants.SEARCH_MOVIES_TYPE`, 'Movies'), (`constants.SEARCH_SHOWS_TYPE`, 'Shows')]
+        program_combo = Gtk.ComboBoxText()
+        for tv_program in tv_programs:
+            program_combo.append(tv_program[0], tv_program[1])
+        program_combo.set_active_id(`constants.SEARCH_ALL_TYPE`)
+        return program_combo
+
+    def _get_entry(self):
+        search_entry = Gtk.Entry()
+        search_entry.set_icon_from_stock(Gtk.EntryIconPosition.SECONDARY, Gtk.STOCK_FIND)
+        search_entry.set_placeholder_text("Search...")
+        search_entry.connect("icon-press", self._on_search_icon_pressed)
+        search_entry.connect("activate", self._on_search_enter_pressed)
+        return search_entry
+
+    def _on_search_icon_pressed(self, program_entry, icon_pos, event):
+        query = self._get_query(program_entry)
+        print "Selected: id=%s, type=%s, text=%s" % (query)
+
+    def _on_search_enter_pressed(self, program_entry):
+        query = self._get_query(program_entry)
+        print "Selected: id=%s, type=%s, text=%s" % (query)
+
+    def _get_query(self, program_entry):
+        return (self.program_combo.get_active_id(), self.program_combo.get_active_text(), program_entry.get_text())
